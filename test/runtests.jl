7d892077623a574107cc6a6da75d13106010879e
Kiize <vitogallo99@gmail.com>
2025-11-21 15:48:56 +0100

    add test for rectangular billiard

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,22 @@
 using QuantumChaos
 using Test
+using ArnoldiMethod
+#using Revise
 
-B = RectBilliard(1.0, 2.0, 100, 200)
+B = RectBilliard(1.0, sqrt(2), 100, 200) # Rectangular Billiard
+ham = rect_laplacian(B) # Hamiltonian
+k = 20  # Number of eigenvalues to compute
 
-@testset "QuantumChaos.jl" begin
-    @test B.Lx == 1.0
-end
+# Diagonalization using Arnoldi method.
+
+decomp, history = partialschur(ham, nev=k, tol=1e-6, which=:SR)  
+history
+E_num, Ψ_vecs = partialeigen(decomp)
+
+E_ana = analytical_energies(B, k)
+
+# Test.
+
+@testset "Comparing numerical to analytical energies" begin
+    @test isapprox(E_num, E_ana; rtol=1e-3)
+end 
